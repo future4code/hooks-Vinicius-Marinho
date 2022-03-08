f2ea3bcaecc94617ecadaa52197b4a3a4bcac8d2
Vinicius Andre Ribeiro Marinho <viniciusandrem22@gmail.com>
2022-03-08 20:04:17 -0300

    projeto-blackjack

---
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -0,0 +1,41 @@
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ * 
+ */
+
+// console.log("Boas vindas os jodo de Blackjack!")
+
+// if (confirm("Quer iniciar uma nova rodada?")){
+   
+//    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+//     const carta1 = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+
+//     const carta2 = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+//     const carta3 = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+//     const pontuacaoUsuario = carta.valor + carta1.valor
+//     const pontuacaoComputador = carta2.valor + carta3.valor
+    
+//     console.log(`Usuário -  cartas: ${carta.texto} ${carta1.texto} - pontuação ${pontuacaoUsuario}`)
+//     console.log(`Computador -  cartas: ${carta2.texto} ${carta3.texto} - pontuação ${pontuacaoComputador}`)
+
+//     if(pontuacaoUsuario > pontuacaoComputador){
+//        console.log("O usuário ganhou!")
+//     } else if(pontuacaoUsuario < pontuacaoComputador){
+//        console.log("O computador ganhou!")
+//     } else 
+//     console.log("Empate!")
+
+// } else {
+//    console.log("O jogo acabou.")
+// }

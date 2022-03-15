4a40a668821e651dc020251c22f5acf4a95983bf
Vinicius Andre Ribeiro Marinho <viniciusandrem22@gmail.com>
2022-03-15 11:57:30 -0300

    revisao-js-exercicio-11

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -133,7 +133,10 @@ function retornaSegundoMaiorESegundoMenor(array) {
 
 // EXERCÍCIO 11
 function retornaChamadaDeFilme(filme) {
-   
+   let quantAtores = filme.atores.length
+  console.log(quantAtores)
+   let chamada = `Venha assistir ao filme ${filme.nome}, de ${filme.ano}, dirigido por ${filme.diretor} e estrelado por ${filme.atores[0]}, ${filme.atores[1]}, ${filme.atores[2]}, ${filme.atores[3]}.`
+   return chamada
 }
 
 // EXERCÍCIO 12

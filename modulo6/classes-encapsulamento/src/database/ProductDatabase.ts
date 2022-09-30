f9c390a2ab4b69a6e3a660ba1af6db6dfb469297
ViniciusMarinho <viniciusandrem22@gmail.com>
2022-09-29 21:42:41 -0300

    exercicios aula heranca-static-abstract

---
+++ modulo6/classes-encapsulamento/src/database/ProductDatabase.ts
@@ -0,0 +1,27 @@
+import { Product } from './../models/Product';
+import { BaseDatabase } from "./BaseDatabase";
+
+export class ProductDatabase extends BaseDatabase{
+    public static TABLE_PRODUCTS = "Labe_Products"
+
+    public async getProducts(){
+        const result = await BaseDatabase.connection(ProductDatabase.TABLE_PRODUCTS)
+        .select()
+        return result        
+    }
+
+    public async createProduct(product: Product){
+        await BaseDatabase.connection(ProductDatabase.TABLE_PRODUCTS).insert({
+            id: product.getId(),
+            name: product.getName(),
+            price: product.getPrice()
+        })
+    }
+
+    public async getProductsById(id:string){
+        const result = await BaseDatabase.connection(ProductDatabase.TABLE_PRODUCTS)
+        .select()
+        .where({ id})
+        return result
+    }
+}

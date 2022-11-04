7df83f7294ff68853f8d9f2a053ac688c49d35e5
ViniciusMarinho <viniciusandrem22@gmail.com>
2022-11-04 14:38:59 -0300

    exercicios criptografia e user roles

---
+++ modulo6/criptografia-e-user-roles/exercicios.md
@@ -0,0 +1,9 @@
+### exercicio 1
+
+a) round é o número de vezes que o codigo fonte passa pelo algoritmo de encriptação, e salt é uma string aleatoria que é adicionada ao codigo antes de criar o hash. Usei o valor de 12 de round, não aumenta muito o tempo de encriptação e aumenta a segurançã da encriptação.
+
+### exercicio 2
+
+a) O de cadastro, pois pra testar o logim, o cadastro tem que ter sido feito.
+
+d) Crio que não, pois só retornamos os ids e emails do usuário.
---
+++ modulo6/criptografia-e-user-roles/src/model/user.ts
@@ -0,0 +1,43 @@
+export enum UserRoles{
+   ADMIN="ADMIN",
+   NORMAL = "NORMAL"
+}
+
+export type user = {
+   id: string
+   email: string
+   password: string
+   name: string
+   nickname: string
+   role: UserRoles
+}
+
+export interface UserInputDTO {
+   name: string,
+   nickname: string,
+   email: string,
+   password: string,
+   role:UserRoles
+}
+
+export interface LoginInputDTO {
+   email: string,
+   password: string
+}
+
+export interface EditUserInputDTO {
+   name: string,
+   nickname: string,
+   id: string,
+   token: string
+}
+
+export interface EditUserInput {
+   name: string,
+   nickname: string,
+   id: string
+}
+
+export interface AuthenticationData {
+   id: string
+}
